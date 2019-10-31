583aa1fa5948b5030aee1d27f9f1fa5bcf991dd9
Mayke Alisson <maykealison@gmail.com>
2019-10-31 06:08:35 -0300

    import project

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:busca_gifs/ui/gif_page.dart';
+import 'package:busca_gifs/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+//  home: GifPage(),
+  ));
+}
+
