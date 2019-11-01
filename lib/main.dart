1123f2c8a6ee47678d63706272b99f2b10dc1e93
Mayke Alisson <maykealison@gmail.com>
2019-11-01 04:47:31 -0300

    finalizando app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'package:busca_gifs/ui/gif_page.dart';
 import 'package:busca_gifs/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
-//  home: GifPage(),
   ));
 }
 
--- lib/ui/gif_page.dart
+++ lib/ui/gif_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
-
   final Map _gifData;
 
   GifPage(this._gifData);
@@ -10,8 +10,15 @@ class GifPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Text(_gifData["title"]),
+        title: Text(_gifData["title"]),
         backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.share),
+              onPressed: () {
+                Share.share(_gifData["images"]["fixed_height"]["url"]);
+              })
+        ],
       ),
       backgroundColor: Colors.black,
       body: Center(
